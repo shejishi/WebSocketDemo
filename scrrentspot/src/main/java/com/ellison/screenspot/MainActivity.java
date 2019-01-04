0bbf1b0f52ace0638f0bd9c792e304f2eced8dca
ellison <ellison.sun0808@outlook.com>
2019-01-04 23:09:36 +0800

    添加截图项目

---
+++ scrrentspot/src/main/java/com/ellison/screenspot/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ellison.screenspot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
